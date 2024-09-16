c0127c52da05fee5bfd385090f11bab600e2ee18
Sebastian <sebastian.varelagi@gmail.com>
2024-09-15 20:23:08 -0500

    Add SVG icons

---
+++ public/icons/SkipIcon.tsx
@@ -0,0 +1,33 @@
+export const SkipIcon = () => {
+	return (
+		<svg
+			width={30}
+			height={30}
+			viewBox="0 0 24 24"
+			className="m-auto"
+			fill="none"
+		>
+			<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+			<g
+				id="SVGRepo_tracerCarrier"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				stroke="#CCCCCC"
+				strokeWidth="0.048"
+			></g>
+			<g id="SVGRepo_iconCarrier">
+				<path
+					d="M16.6598 9.35258C18.4467 10.5065 18.4467 13.4935 16.6598 14.6474L5.87083 21.6145C4.13419 22.736 2 21.2763 2 18.9671L2 5.0329C2 2.72368 4.13419 1.26402 5.87083 2.38548L16.6598 9.35258Z"
+					stroke="#ffffff"
+					strokeWidth="1.5"
+				></path>{' '}
+				<path
+					d="M22 5V19"
+					stroke="#ffffff"
+					strokeWidth="1.5"
+					strokeLinecap="round"
+				></path>
+			</g>
+		</svg>
+	);
+};
