686ba63c8e8b0455ce65b763bcf6c9553b6f0685
Sebastian <sebastian.varelagi@gmail.com>
2024-09-15 20:20:52 -0500

    Replace 'Header' for 'Menu'

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -2,17 +2,11 @@
 
 import { MouseEvent, useEffect, useState } from "react"
 
-export const Header = () => {
+export const Menu = () => {
 
   const [dimension, setDimension] = useState({
     width: 0, left: 0
   })
- 
-  useEffect(() => {
-    window.addEventListener('resize', getDimension)
- 
-    return () => window.removeEventListener('resize', getDimension)
-  }, [])
 
   const getDimension = () =>{
     const {offsetWidth, offsetLeft} = document.querySelector("button.active") as HTMLButtonElement
@@ -30,14 +24,19 @@ export const Header = () => {
     })
   }
 
+ 
+  useEffect(() => {
+    window.addEventListener('resize', getDimension)
+    return () => window.removeEventListener('resize', getDimension)
+  }, [])
+
   useEffect(() => {
     getDimension()
   }, [])
 
   return (
-    <header>
-      <nav className="flex flex-col items-center w-full min-w-fit h-20">
-        <ul className="flex justify-center items-center text-md md:text-xl bg-foreground relative">
+      <div className="flex justify-center w-full min-w-fit h-20 relative">
+        <ul className="flex text-md md:text-xl bg-white/10 relative">
           <li>
             <button 
               className="w-full h-20 px-6 md:px-8 opacity-75 border-b-4 border-transparent transition-all delay-100 hover:border-b-4 hover:border-white/40 active"
@@ -64,7 +63,6 @@ export const Header = () => {
           </li>
           <span className="animation" style={{width: dimension.width, left: dimension.left}}></span>
         </ul>
-      </nav>
-    </header>
+      </div>
   )
 }
