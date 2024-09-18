d9af60d9c51e81c614dc73a223c157deb9c3b13b
Sebastian <sebastian.varelagi@gmail.com>
2024-09-18 11:46:55 -0500

    Add events in useCountdown helper

---
+++ src/utils/useCountdown.ts
@@ -0,0 +1,75 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+type props = {
+  minutes: number;
+  onInit?: () => void;
+  onStop?: () => void;
+  onFinish?: () => void;
+}
+
+export default function useCountdown({minutes, onInit, onStop, onFinish} : props){
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const time = minutes * 60;
+  const [timeLeft, setTimeLeft] = useState(time);
+  const [ticking, setTicking] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  const clear = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+  }
+
+  const tick = useCallback(() =>{
+    if (timeLeft > 0){
+      setTimeLeft(timeLeft - 1);
+      setProgress((count) => count + 1); // Sum every second
+    }
+    if(timeLeft < 1){
+      setTicking(false);
+      setProgress(0);
+      onFinish?.();
+      clear();
+    }
+  }, [timeLeft, onFinish])
+  
+  useEffect(() =>{
+    if(ticking){
+      intervalRef.current = setInterval(tick, 1000);
+    }else{
+      clear();
+    }
+    return clear;
+  }, [tick, ticking]);
+
+  useEffect(() =>{
+    setTimeLeft(time);
+  }, [time]);
+
+  const start = useCallback(() =>{
+    setTicking(true);
+    onInit?.();
+  }, [onInit])
+  
+  const stop = useCallback(() =>{
+    setTicking(false);
+    onStop?.();
+  }, [onStop])
+
+  const reset = useCallback(() =>{
+    setTicking(false);
+    setProgress(0);
+    setTimeLeft(time);
+    clear();
+  }, [time])
+
+  return{
+    timeLeft,
+    progress: progress / time,
+    ticking,
+    start,
+    stop,
+    reset,
+  }
+}
