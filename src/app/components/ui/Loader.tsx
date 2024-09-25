81d9001b3a68ace948d0abf460aa3735b0f9aa01
Sebastian <sebastian.varelagi@gmail.com>
2024-09-25 18:24:40 -0500

    Adjust loader height

--- src/app/components/ui/Loader.tsx
+++ src/app/components/ui/Loader.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 
 export const Loader = () => {
   return (
-    <>
-      <div className="min-w-screen max-h-screen flex justify-center items-center">
-				<div className="w-[200px] h-[200px] inline-block bg-none">
-					<div className="w-full h-full relative loader">
-						<div className='absolute w-[100px] h-[100px] border-[5px] border-white/75 border-t-transparent rounded-[50%] animate-loader top-[100px] left-[100px] box-content'></div>
-					</div>
+		<div className="min-w-screen min-h-screen flex justify-center items-center bg-backgroundPomo">
+			<div className="w-[200px] h-[200px] inline-block bg-none">
+				<div className="w-full h-full relative loader">
+					<div className='absolute w-[100px] h-[100px] border-[5px] border-white/75 border-t-transparent rounded-[50%] animate-loader top-[100px] left-[100px] box-content'></div>
 				</div>
 			</div>
-    </>
+		</div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { Timer } from "./components/Timer";
 import clsx from "clsx";
 
 export default function Home() {
-  
   const { mode } = useAppSelector(state => state.timer)
 
     const containerClasses = clsx("min-w-screen min-h-screen", {
